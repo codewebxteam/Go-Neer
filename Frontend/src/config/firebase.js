1b05dcb04a1b70edfc70aa62d3e393521e0d6927
Chandrashekharsk <cs9981625252@gmail.com>
2026-01-30 13:45:30 +0530

    fixed minor

--- Frontend/src/config/firebase.js
+++ Frontend/src/config/firebase.js
@@ -3,19 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: import.meta.env.VITE_FIREBASE_APP_ID,
-
-  apiKey: "AIzaSyBNMXnDdBz4hST37vMfTSQ3XqhzI8mFCL8",
-  authDomain: "fir-demo-2eb75.firebaseapp.com",
-  projectId: "fir-demo-2eb75",
-  storageBucket: "fir-demo-2eb75.firebasestorage.app",
-  messagingSenderId: "987371827994",
-  appId: "1:987371827994:web:9478ebae40c61e892372a3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
